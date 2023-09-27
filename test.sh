a0ef405fbb8190aa717d24e355498b7a9134ae58
peccu <peccul@gmail.com>
2023-09-28 00:38:32 +0900

    add test script using playwright

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ./docker-build.sh bun .codesandbox
+
+echo port mapping is 9323:9323
+echo test with "npm run build && CI=1 npm run test:e2e"
+
+docker run \
+       -it \
+       --rm \
+       --name playwright \
+       -v "$PWD:/app" \
+       -p 9323:9323 \
+       -w /app \
+       --ipc=host \
+       mcr.microsoft.com/playwright:v1.38.0-jammy \
+       /bin/bash
+
