959985eb68eed1899161f1b553d3ad97ad5c761f
peccu <peccul@gmail.com>
2023-09-28 00:05:44 +0900

    add --host for preview

--- shell.sh
+++ shell.sh
@@ -2,12 +2,13 @@
 
 ./docker-build.sh bun .codesandbox
 
-echo port mapping is 5555:5173
+echo port mapping is 5555:5173 4173:4173
 docker run \
        -it \
        --rm \
        --name bun-shell \
        -v "$PWD:/app" \
+       -p 4173:4173 \
        -p 5555:5173 \
        -w /app \
        --entrypoint /bin/bash \
