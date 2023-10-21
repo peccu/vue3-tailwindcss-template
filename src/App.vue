bf06326af1f27ef8b1d91b9ab3dfaa96ddb9cdad
peccu <peccul@gmail.com>
2023-10-21 20:07:11 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import { ref } from 'vue'
-const number = ref(0)
 </script>
 
 <template>
