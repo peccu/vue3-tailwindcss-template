9d0e140085bbe1ff030e99fd72bbaec722975643
peccu <peccul@gmail.com>
2023-10-22 23:31:32 +0900

    partially move to tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,15 +8,15 @@ const number = ref(0)
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }} with number {{ number }}</h1>
+    <h1 class="relative font-medium leading-normal -top-[10px] text-[2.6rem] green">{{ msg }} with number {{ number }}</h1>
     <input
       inputmode="numeric"
       pattern="[0-9]*"
       type="text"
-      class="p-1 mr-3 w-20 text-black w-full"
+      class="p-1 mr-3 w-20 w-full text-black"
       v-model="number"
     />
-    <h3>
+    <h3 class="text-[1.2rem]">
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
@@ -25,17 +25,6 @@ const number = ref(0)
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
 .greetings h1,
 .greetings h3 {
   text-align: center;
