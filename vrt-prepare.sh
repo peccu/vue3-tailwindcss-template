64ddb52d5b5b43e9491df9562e239be0dfd9c615
peccu <peccul@gmail.com>
2023-10-22 23:48:32 +0900

    fix typo and add -it

--- vrt-prepare.sh
+++ vrt-prepare.sh
@@ -3,8 +3,9 @@
 ./docker-build.sh bun .codesandbox
 
 docker run \
+       -it \
        --rm \
-       --name bun-vrt-pepare \
+       --name bun-vrt-prepare \
        --mount type=bind,source="$(pwd)",target=/app \
        -w /app \
        --entrypoint /bin/bash \
