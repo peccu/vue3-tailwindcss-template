765520bd70c85e247577a0fa0231c386bb152f65
peccu <peccul@gmail.com>
2023-11-04 11:05:15 +0900

    add comment

--- vrt-prepare.sh
+++ vrt-prepare.sh
@@ -4,6 +4,7 @@ CONTAINER_NAME=bun-vrt-prepare
 COMMAND="bun run test:vrt:prepare"
 PORT_MAPPINGS=""
 # TODO need to install packages in below volume
+# move this into default option?
 VOLUMES="-v $(PWD)/vrt-node-modules:/app/node_modules"
 
 source ./bun-container.sh
