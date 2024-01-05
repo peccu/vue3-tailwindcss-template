3ba4f0ce106eef0de11b46edcebdb8a25282dee6
peccu <peccul@gmail.com>
2024-01-05 20:21:27 +0900

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,13 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'ignorePatterns': ['*.config.js', '!.storybook'],
-  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting', 'plugin:storybook/recommended'],
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:storybook/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
   }
