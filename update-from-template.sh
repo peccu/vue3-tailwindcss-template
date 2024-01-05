a1cd53f579b137dcce64f8627f723bdd5f2f72d0
peccu <peccul@gmail.com>
2024-01-05 14:02:28 +0900

    update update script

--- update-from-template.sh
+++ update-from-template.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
 
 export APP=vue-app
-# bun create vue@latest ${APP} --ts --vue-router --pinia --vitest --playwright --eslint-with-prettier --force
+bun create vue@latest ${APP} --ts --vue-router --pinia --vitest --playwright --eslint-with-prettier --force
 
-# cd ${APP}
+cd ${APP}
 
-# if [ $(ulimit -n) -lt 300 ]
-# then
-#     echo need to increase maxfiles
-#     sudo launchctl limit maxfiles 2147483646
-#     ulimit -n 2147483646
-# fi
+if [ $(ulimit -n) -lt 300 ]
+then
+    echo need to increase maxfiles
+    sudo launchctl limit maxfiles 2147483646
+    ulimit -n 2147483646
+fi
 
-# bun i
+bun i
 
-# bun add -d @vitest/coverage-v8 @vitest/ui
-# bun add -d dependency-cruiser
-# bun add -d tailwindcss postcss autoprefixer
+bun add -d @vitest/coverage-v8 @vitest/ui
+bun add -d dependency-cruiser
+bun add -d tailwindcss postcss autoprefixer
+
+bunx tailwindcss init -p
+# make this patch from (R means reverse, no-prefix means no a/, b/ prefix in filepath)
+# cf. https://stackoverflow.com/a/4610809/514411
+# git diff -R --no-prefix src/assets/base.css > patch/tailwind.patch
+patch -p0 < ../patch/tailwind.patch
+
+# This uses node.js and npm. not supported bun yet
+bunx storybook@latest init --disable-telemetry --package-manager bun --skip-install --yes
+# does not supports storybook 7
 # bun add -d @storybook/addon-postcss
-# bun add -d oxlint
+# dont used?
+bun add -d eslint-plugin-storybook
 
-# bunx tailwindcss init -p
+bun add -d oxlint
 
-# # This uses node.js and npm. not supported bun yet
-# bunx storybook@latest init --disable-telemetry --package-manager bun --yes
+bun run format
+bun run eslint
 
+# git diff -R --no-prefix .eslintrc.cjs e2e/tsconfig.json e2e/vue.spec.ts index.html playwright.config.ts tailwind.config.js vite.config.ts vitest.config.ts > patch/my-config.patch
+patch -p0 < ../patch/my-config.patch
 
-# bun run format
-# bun run eslint
+rm -rf node_modules
 
-# # TODO apply patch for my conf
-# # make this patch from (R means reverse, no-prefix means no a/, b/ prefix in filepath)
-# # git diff -R --no-prefix src/assets/base.css > patch/tailwind.patch
-# patch -p0 < ../tailwind.patch
-
-# # git diff -R --no-prefix .eslintrc.cjs e2e/tsconfig.json e2e/vue.spec.ts index.html playwright.config.ts tailwind.config.js vite.config.ts vitest.config.ts > patch/my-config.patch
-# patch -p0 < ../my-config.patch
-
-# rm -rf node_modules
-
-# cd ..
+cd ..
 
 # TODO apply patch from git diff
 cp -R $(
     ls -a1 $APP \
-	| grep -Ev '^\.\.?$' \
-	| grep -Ev '^\.git$' \
-	| grep -Ev '^'$APP'$' \
-	| xargs -I{} echo "$APP/"{}
+        | grep -Ev '^\.\.?$' \
+        | grep -Ev '^\.git$' \
+        | grep -Ev '^'$APP'$' \
+        | xargs -I{} echo "$APP/"{}
    ) ./
