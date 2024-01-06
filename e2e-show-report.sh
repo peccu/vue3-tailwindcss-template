4a3555963b79fdca27300f785a44202bfe10d2d2
peccu <peccul@gmail.com>
2024-01-06 09:52:21 +0900

    split e2e test and report

--- e2e-show-report.sh
+++ e2e-show-report.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo port mapping is 9323:9323 for playwright show-report
-echo 'test with "npm run build && npm run test:e2e"'
+echo 'show test report with "npm run test:e2e:show-report"'
 echo 'It includes running "playwright show-report"'
 # https://playwright.dev/docs/ci-intro
 
 CONTAINER_NAME=playwright
-COMMAND="bun run build && bun run test:e2e"
+COMMAND="bun run test:e2e:show-report"
 PORT_MAPPINGS="-p 9323:9323"
 VOLUMES="-v $(PWD)/node_modules_container:/app/node_modules"
-ENVIRONMENT="--env CI=true"
+ENVIRONMENT=""
 
 source container.sh
 if [ ! -d node_modules_container ]
