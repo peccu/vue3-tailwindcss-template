bfd186001ac367f981520fafa452dff4164bb96a
peccu <peccul@gmail.com>
2024-01-07 13:49:36 +0900

    update templates

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -2,25 +2,12 @@
 defineProps<{
   msg: string
 }>()
-import { ref } from 'vue'
-const number = ref(0)
 </script>
 
 <template>
   <div class="greetings">
-    <h1
-      class="relative font-medium leading-normal text-center lg:text-left -top-[10px] text-[2.6rem] green"
-    >
-      {{ msg }} with number {{ number }}
-    </h1>
-    <input
-      inputmode="numeric"
-      pattern="[0-9]*"
-      type="text"
-      class="p-1 mr-3 w-20 w-full text-black"
-      v-model="number"
-    />
-    <h3 class="text-center lg:text-left text-[1.2rem]">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
@@ -28,4 +15,27 @@ const number = ref(0)
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
